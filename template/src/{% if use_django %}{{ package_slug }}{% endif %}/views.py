ed32e8e3e00aa6f09ec40365aa79350b294fbb8f
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2026-08-26 08:12:25 +0000

    Remove Django support from project scaffolding and templates

--- template/src/{% if use_django %}{{ package_slug }}{% endif %}/views.py
+++
@@ -1,8 +0,0 @@
-"""Views."""
-
-from django.http import HttpRequest, HttpResponse
-
-
-def index(request: HttpRequest) -> HttpResponse:
-    """Index view."""
-    return HttpResponse("Hello Django world")
